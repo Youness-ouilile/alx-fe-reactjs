f01ffb49a23148d19bc19afa4f3235dd60ffd5bf
Youness-ouilile <ouilileyouness@gmail.com>
2024-11-16 16:15:48 +0100

    new app react

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const UserProfile = (props) => {
   return (
-    <div style={{ border: '2px solid gray', padding: '12px', margin: '12px' }}>
-      <h2 style={{ color: 'red' }}>Name: {props.name}</h2>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2 style={{ color: 'bleu' }}>Name: {props.name}</h2>
       <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
       <p>Bio: {props.bio}</p>
       
