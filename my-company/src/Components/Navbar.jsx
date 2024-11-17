b57785537c1a433b1f3d4826988558adf5eab66a
Youness-ouilile <ouilileyouness@gmail.com>
2024-11-17 23:53:37 +0100

    my company

--- my-company/src/Components/Navbar.jsx
+++ my-company/src/Components/Navbar.jsx
@@ -3,37 +3,23 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={styles.navbar}>
-      <ul style={styles.navList}>
-        <li style={styles.navItem}><Link to="/" style={styles.navLink}>Home</Link></li>
-        <li style={styles.navItem}><Link to="/about" style={styles.navLink}>About</Link></li>
-        <li style={styles.navItem}><Link to="/contact" style={styles.navLink}>Contact</Link></li>
-        <li style={styles.navItem}><Link to="/services" style={styles.navLink}>Services</Link></li>
+    <nav style={{ padding: '10px', backgroundColor: '#333', color: 'white' }}>
+      <ul style={{ listStyleType: 'none', display: 'flex', justifyContent: 'space-around' }}>
+        <li>
+          <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
+        </li>
+        <li>
+          <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
+        </li>
+        <li>
+          <Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link>
+        </li>
+        <li>
+          <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
+        </li>
       </ul>
     </nav>
   );
 }
-
-const styles = {
-  navbar: {
-    backgroundColor: '#333',
-    padding: '10px',
-  },
-  navList: {
-    display: 'flex',
-    listStyle: 'none',
-    justifyContent: 'center',
-    margin: 0,
-    padding: 0,
-  },
-  navItem: {
-    margin: '0 15px',
-  },
-  navLink: {
-    color: '#fff',
-    textDecoration: 'none',
-    fontSize: '18px',
-  },
-};
 
 export default Navbar;
