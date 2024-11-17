04bca1f86b8cbace21c4948e6bec2e879d6ebb81
Youness-ouilile <ouilileyouness@gmail.com>
2024-11-17 23:25:49 +0100

    my compnay

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-import Navbar from './Components/Navbar'; 
+import Navbar from '../my-company/src/Components/Navbar'; 
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
