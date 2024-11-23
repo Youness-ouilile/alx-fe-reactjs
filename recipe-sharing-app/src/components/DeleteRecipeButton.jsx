46ee8bd2cf52e1c7b8fb2442f9795739cca6c6e6
Youness-ouilile <ouilileyouness@gmail.com>
2024-11-23 21:44:32 +0100

    zustand

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,12 +1,13 @@
 import { useRecipeStore } from './recipeStore'; 
-
+import { useNavigate } from 'react-router-dom';
 const DeleteRecipeButton = ({ recipeId }) => {
- 
+    const navigate = useNavigate();
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
   const handleDelete = () => {
     
     deleteRecipe(recipeId);
+    navigate('/recipes');
   };
 
   return (
