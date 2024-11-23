fa39e6434f00f5630a0052c5b8312664f2b5146c
Youness-ouilile <ouilileyouness@gmail.com>
2024-11-23 20:26:28 +0100

    niice

---
+++ recipe-sharing-app/recipe-sharing-app/src/Component/AddRecipeForm.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { useRecipeStore } from '../recipeStore';
+
+const AddRecipeForm = () => {
+  const addRecipe = useRecipeStore((state) => state.addRecipe);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (title && description) {
+      addRecipe({ id: Date.now(), title, description });
+      setTitle('');
+      setDescription('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Recipe Title"
+        />
+      </div>
+      <div>
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Recipe Description"
+        />
+      </div>
+      <button type="submit">Add Recipe</button>
+    </form>
+  );
+};
+
+export default AddRecipeForm;
