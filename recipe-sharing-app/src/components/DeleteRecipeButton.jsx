fb3f7690ef20360566c716dab69ab584b7b91448
Youness-ouilile <ouilileyouness@gmail.com>
2024-11-23 21:39:04 +0100

    zustand

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,11 @@
-import { useRecipeStore } from './recipeStore'; 
+import { useNavigate } from 'react-router-dom';
 
 const DeleteRecipeButton = ({ recipeId }) => {
-  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe); 
+  const navigate = useNavigate();
 
   const handleDelete = () => {
-    
-    deleteRecipe(recipeId);
+ 
+    navigate('/recipes'); 
   };
 
   return (
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,14 +2,26 @@ import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, newRecipe],
-    })),
+  
+
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, newRecipe],
+  })),
+  
+  
   setRecipes: (recipes) => set({ recipes }),
-  deleteRecipe: (recipeId) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
-    })),
+
+  
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+  })),
+
+  
+  deleteRecipe: (recipeId) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+  })),
 }));
+
 export default useRecipeStore;
