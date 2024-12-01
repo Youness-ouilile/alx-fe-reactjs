18cf8fc816b52642eef0035806911cb8e824f667
Youness-ouilile <ouilileyouness@gmail.com>
2024-12-01 21:35:32 +0100

    farmiik

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import formikForm from './components/formikForm'
+import formikForm from './components/FormikForm'
 
 function App() {
   const [count, setCount] = useState(0)
