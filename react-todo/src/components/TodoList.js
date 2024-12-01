561305b4082f53150a9bdc0dff510ac049b9ca66
Youness-ouilile <ouilileyouness@gmail.com>
2024-12-01 23:03:10 +0100

    todo

--- react-todo/src/components/TodoList.js
+++ react-todo/src/components/TodoList.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 
 const TodoList = () => {
-  // Initialize state with some demo todos
   const [todos, setTodos] = useState([
     { id: 1, text: 'Learn React', completed: false },
     { id: 2, text: 'Build Todo App', completed: false },
   ]);
   const [newTodo, setNewTodo] = useState('');
 
-  // Add a new todo
   const addTodo = () => {
     if (newTodo.trim()) {
       setTodos([...todos, { id: Date.now(), text: newTodo, completed: false }]);
@@ -16,14 +14,12 @@ const TodoList = () => {
     }
   };
 
-  // Toggle the completed state of a todo
   const toggleTodo = (id) => {
     setTodos(todos.map(todo => 
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
     ));
   };
 
-  // Delete a todo
   const deleteTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id));
   };
