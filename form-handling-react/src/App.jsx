181efb4b32627638efb66cb02a8d673548cdcfd8
Youness-ouilile <ouilileyouness@gmail.com>
2024-12-01 21:33:55 +0100

    lormik

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import formikForm from './components/FormikForm'
+import formikForm from './components/formikForm'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,4 +14,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
--- form-handling-react/src/components/formikForm.jsx
+++ form-handling-react/src/components/formikForm.jsx
@@ -3,14 +3,14 @@ import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const formikForm = () => {
-  // Validation schema using Yup
+  
   const validationSchema = Yup.object({
     username: Yup.string().required('Username is required'),
     email: Yup.string().email('Invalid email format').required('Email is required'),
     password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
   });
 
-  // Handle form submission
+  
   const handleSubmit = (values) => {
     console.log('Form submitted with:', values);
   };
