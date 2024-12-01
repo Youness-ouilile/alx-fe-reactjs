7cf2f4c06a9716b0905cb3685d1567c4aea6222d
Youness-ouilile <ouilileyouness@gmail.com>
2024-12-01 21:48:49 +0100

    query

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './PostsComponent';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+
+const queryClient = new QueryClient();
+<ReactQueryDevtools initialIsOpen={false} />
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <QueryClientProvider client={queryClient}>
+    <PostsComponent /> 
+  </QueryClientProvider>
+  
+  );
+}
+
+export default App;
---
+++ react-query-demo/src/PostsComponent.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+
+
+const fetchPosts = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!res.ok) {
+    throw new Error('Failed to fetch posts');
+  }
+  return res.json();
+};
+
+const PostsComponent = () => {
+  const { data, error, isLoading, isError, isFetching, refetch } = useQuery('posts', fetchPosts, {
+    staleTime: 5 * 60 * 1000, 
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {isFetching && <p>Updating...</p>}
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
