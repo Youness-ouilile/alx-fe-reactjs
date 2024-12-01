cd2498a2d97e59f9999a2112bcbad7c71357bc3b
Youness-ouilile <ouilileyouness@gmail.com>
2024-12-01 22:02:36 +0100

    router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import About from './pages/About';
-import Profile from './pages/Profile';
+import Profile from './components/Profile';
 import NotFound from './pages/NotFound';
 
 function App() {
