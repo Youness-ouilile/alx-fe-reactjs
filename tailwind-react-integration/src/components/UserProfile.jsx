b3165c74f9366f8d1cca9d3a00e931c4cdd1c3af
Youness-ouilile <ouilileyouness@gmail.com>
2024-12-08 12:18:33 +0100

    zailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-sm sm:max-w-xs md:max-w-md mx-auto my-20 rounded-lg shadow-lg">
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
-        className="rounded-full w-24 h-24 sm:w-36 sm:h-36 md:w-48 md:h-48 mx-auto mb-4" 
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto mb-4" 
       />
-      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-sm sm:text-base md:text-lg text-gray-600">
+      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <p className="text-sm sm:text-sm md:text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
