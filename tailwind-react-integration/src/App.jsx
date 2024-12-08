8e296c1d2cac3fb79900c19e171a47cd5d69c1d3
Youness-ouilile <ouilileyouness@gmail.com>
2024-12-08 12:40:34 +0100

    zailwind

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import UserProfile from './components/UserProfile'
-
+import './index.css';
 function App() {
   const [count, setCount] = useState(0)
 
